8b7e348ce99439940620b164c575df7c58d4842e
Suhail Kakar <suhail.zahed@gmail.com>
2024-04-25 13:05:05 +0530

    test: add stream tests

--- tests/stream.test.ts
+++ tests/stream.test.ts
@@ -64,4 +64,56 @@ describe("Streams API", () => {
       expect(isStructureEqual).toBe(true);
     });
   });
+
+  describe("List streams", () => {
+    it("should list all streams and match the structure of direct API call", async () => {
+      const { data: streams } = await sdk.stream.getAll();
+
+      const response = await fetch(`${API_BASE_URL}/stream`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`API responded with status ${response.status}`);
+      }
+
+      const directApiResponse = await response.json();
+
+      const isStructureEqual = compareResponseStructures(
+        streams,
+        directApiResponse
+      );
+      expect(isStructureEqual).toBe(true);
+    });
+  });
+
+  describe("Update a stream", () => {
+    it("should update a stream and match the structure of direct API call", async () => {
+      const streamId = "b1aac6bc-6a2c-41d3-a95c-0c41864ddd4e";
+      const body = {
+        record: true,
+      };
+
+      const { statusCode } = await sdk.stream.update(streamId, body);
+
+      const response = await fetch(`${API_BASE_URL}/stream/${streamId}`, {
+        method: "PATCH",
+        body: JSON.stringify(body),
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`API responded with status ${response.status}`);
+      }
+
+      expect(statusCode).toBe(204);
+      expect(response.status).toBe(204);
+    });
+  });
 });
