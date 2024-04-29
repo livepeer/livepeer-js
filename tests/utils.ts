c45ec9298f7ed57c17ae943bb126fc519aee5f09
Suhail Kakar <suhail.zahed@gmail.com>
2024-04-29 09:53:29 +0530

    refactor: add comments to utils

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,3 +1,10 @@
+/**
+ * Utility function to compare the structures of two objects
+ * @param {Object} obj1 - The first object to compare
+ * @param {Object} obj2 - The second object to compare
+ * @returns {boolean} Returns true if the structures of obj1 and obj2 are same, otherwise false.
+ */
+
 const compareResponseStructures = (obj1: any, obj2: any) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
